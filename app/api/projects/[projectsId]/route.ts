2facfdff13dea37e9fef097cc3fecee3d36c10fe
Tanujeet <singhtanujeet@gmail.com>
2025-07-11 09:43:34 +0530

    Added :projects api

---
+++ app/api/projects/[projectsId]/route.ts
@@ -0,0 +1,41 @@
+import prisma from "@/lib/primsa";
+import { auth } from "@clerk/nextjs/server";
+
+
+import { NextResponse } from "next/server";
+
+export async function GET(
+  req: NextResponse,
+  context: { params: { projectId: string } }
+) {
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("Unauthorised", { status: 402 });
+  }
+
+
+  const { projectId } = context.params;
+
+  const fetchProject = await prisma.project.findUnique({
+    where: { id: projectId },
+    include: { slides: true, collaborators: { include: { user: true } } },
+  });
+
+  if (!fetchProject) {
+    return new NextResponse("Project not found", { status: 404 });
+  }
+   
+
+  const isOwner = fetchProject.ownerId === userId;
+  const isCollaborator = fetchProject.collaborators.some(
+    (c) => c.userId === userId
+  );
+
+
+  if (!isOwner && !isCollaborator) {
+    return new NextResponse("Forbidden", { status: 403 });
+  }
+
+  return NextResponse.json(fetchProject);
+
+}
---
+++ app/api/projects/route.ts
@@ -0,0 +1,35 @@
+import prisma from "@/lib/primsa";
+import { auth } from "@clerk/nextjs/server";
+import { nanoid } from "nanoid";
+
+import { NextResponse } from "next/server";
+
+
+
+
+export async function POST(req: NextResponse) {
+    const { userId } = await auth();
+   
+    if (!userId) {
+        return new NextResponse("Unauthorised", { status: 401 });
+      }
+    try {
+        const body = await req.json();
+        const { title, description } = body;
+        if (!title || typeof title !== "string") {
+          return new NextResponse("Invalid title", { status: 400 });
+        }
+        
+        const newProject = await prisma.project.create({
+          data: {
+            title,
+            description,
+            ownerId: userId,
+            shareId: nanoid(8),
+          },
+        });
+        return NextResponse.json(newProject, { status: 200 });
+    } catch (err) {
+        console.error(err)
+    }
+} 
