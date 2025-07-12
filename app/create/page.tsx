55337b0b7843021d36beee4dce9e6f8ad862a10f
Tanujeet <singhtanujeet@gmail.com>
2025-07-12 10:26:25 +0530

    changes :create page added

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,3 +1,6 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
 const page = () => {
   return (
     <main>
@@ -9,6 +12,14 @@ const page = () => {
             </h1>
             <p className="text-xl mt-4 font-medium">
               Powered by AI,Built for visionaries
+            </p>
+            <Textarea
+              placeholder="An app that connects local artists with businesses for mural projects..."
+              className="mt-10 h-40 text-base"
+            />
+            <Button className="mt-10">Generate Slides</Button>
+            <p className="font-light mt-4">
+              Tip:Write your idea like your talking to an investor or user
             </p>
           </div>
         </div>
