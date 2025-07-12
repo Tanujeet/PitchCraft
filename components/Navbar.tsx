20ddc78f88a8236aea9f8b60a01949e8d1e802b3
Tanujeet <singhtanujeet@gmail.com>
2025-07-12 07:53:44 +0530

    changes :Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,26 +5,25 @@ import Link from "next/link";
 import { useUser } from "@clerk/nextjs";
 
 const Navbar = () => {
-  const navLinks = [
-    { name: "Product", href: "/Product" },
-    { name: "Pricing", href: "/Pricing" },
-    { name: "Resources", href: "/Resources" },
-  ];
+
   const { user, isLoaded } = useUser();
   return (
     <nav className="h-20 border-b border-black px-6 md:px-10 flex items-center justify-between">
-      <Link href="/" className="text-2xl font-bold tracking-tight">
+      <Link href="/dashboard" className="text-2xl font-bold tracking-tight">
         Pitch<span className="text-blue-600">Craft</span>
       </Link>
 
       <ul className="flex gap-6 items-center text-lg">
-        {navLinks.map((link, idx) => (
-          <li key={idx}>
-            <Link href={link.href} className="hover:underline">
-              {link.name}
-            </Link>
-          </li>
-        ))}
+        {user && (
+          <>
+            <li>
+              <Link href="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link href="/create">New Project</Link>
+            </li>
+          </>
+        )}
         <li>
           {isLoaded && user ? (
             <UserButton afterSignOutUrl="/" />
