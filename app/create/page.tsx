1c2330563940b4bbd168c0bdf556169450636764
Tanujeet <singhtanujeet@gmail.com>
2025-07-12 10:20:27 +0530

    changes :create page added

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,6 +1,19 @@
 const page = () => {
   return (
-    <div>page</div>
+    <main>
+      <section>
+        <div className="flex justify-center items-center mt-20">
+          <div className="text-center">
+            <h1 className="text-4xl font-bold">
+              Describe Your idea.Let AI Craft your pitch
+            </h1>
+            <p className="text-xl mt-4 font-medium">
+              Powered by AI,Built for visionaries
+            </p>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 };
 
