5b04fa1048b1d0e7dcb4a770def3a72fc3bef0f6
Tanujeet <singhtanujeet@gmail.com>
2025-07-20 08:02:55 +0530

    Added :export to pdf

--- app/api/slides/export/route.ts
+++ app/api/slides/export/route.ts
@@ -1,12 +1,42 @@
+import { generatePdfFromSlides } from "@/lib/PdfGenerator";
+import prisma from "@/lib/prisma";
 import { auth } from "@clerk/nextjs/server";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-    const { userId } = await auth()
-    if (!userId) {
-        return new NextResponse("Unauthorised", { status: 401 })
-    }
-    
-    const { searchParams } = new URL(req.url)
-    const projectId= searchParams.get("projectID")
+  const { userId } = await auth();
+  if (!userId) {
+    return new NextResponse("Unauthorised", { status: 401 });
+  }
+
+  const { searchParams } = new URL(req.url);
+  const projectId = searchParams.get("projectId");
+  if (!projectId) {
+    return new NextResponse("failed to get projectId", { status: 401 });
+  }
+  try {
+    const slides = await prisma.slide.findMany({
+      where: { projectId },
+      orderBy: { order: "asc" },
+    });
+    const formattedSlides = slides.map((slide) => ({
+      ...slide,
+      content:
+        typeof slide.content === "string"
+          ? slide.content
+          : JSON.stringify(slide.content || ""),
+    }));
+
+    const pdf = await generatePdfFromSlides(formattedSlides);
+    return new NextResponse(new Uint8Array(pdf), {
+      status: 200,
+      headers: {
+        "Content-Type": "application/pdf",
+        "Content-Disposition": `attachment; filename="slides.pdf"`,
+      },
+    });
+  } catch (e) {
+    console.error("PDF generation failed", e);
+    return new NextResponse("Internal Server Error", { status: 500 });
+  }
 }
